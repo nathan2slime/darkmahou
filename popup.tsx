16481595d2362127908d29ecac7fa9f0d5e0abc6
Jhonathan M <nathan3boss@gmail.com>
2025-01-19 01:13:09 -0300

    feat: create service for get anime data

--- popup.tsx
+++ popup.tsx
@@ -1,55 +1,36 @@
-import { createTRPCProxyClient } from '@trpc/client'
-import { useEffect, useState } from 'react'
-import { chromeLink } from 'trpc-chrome/link'
+import { RefreshCw } from 'lucide-react'
+import { useState } from 'react'
 
 import { Button } from '~components/button'
+import { Head } from '~components/head'
 
-import type { AppRouter } from '~/background'
+import { type Anime, fetchAnime } from '~services/anime.service'
 
 import '~/styles.css'
 
-import { Head } from '~components/head'
-
-const port = chrome.runtime.connect()
-const _trpc = createTRPCProxyClient<AppRouter>({
-  links: [chromeLink({ port })]
-})
-
-function IndexPopup() {
-  const [data, setData] = useState('')
-  useEffect(() => {}, [])
+const IndexPopup = () => {
+  const [anime, setAnime] = useState<Anime>()
 
   return (
     <div className="p-4 bg-tsu-base rose-pine min-w-[350px]">
       <Head />
 
-      <Button
-        className="w-full my-4"
-        onClick={async () => {
-          const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
-          const tab = tabs[0]
-
-          chrome.scripting.executeScript(
-            {
-              target: { tabId: tab.id },
-              func: () => {
-                const title = document.querySelector('.entry-title')
-
-                return title.textContent
-              }
-            },
-            res => setData(res[0].result)
-          )
-
-          if (tab) {
-            return tab.id
-          }
-        }}
-      >
+      <Button className="w-full my-4 flex items-center gap-2 justify-center" onClick={async () => setAnime(await fetchAnime())}>
+        <RefreshCw className="w-4 h-4" />
         Load
       </Button>
 
-      {data}
+      {anime && (
+        <div className="w-full bg-tsu-overlay p-2 gap-2 rounded-lg items-start justify-start flex">
+          <img src={anime.thumb} alt={anime.title} className="w-full rounded-lg max-w-[120px]" />
+
+          <div className="flex flex-col">
+            <h1 className="text-base text-tsu-love font-bold text-center line-clamp-3">{anime.title}</h1>
+            <div className="h-[1px] bg-tsu-hhigh my-1 w-full" />
+            <p className="text-xs text-tsu-text font-bold line-clamp-5 text-center">{anime.description}</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
